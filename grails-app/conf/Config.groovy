17400ad831b4d5fe23993227ba57e4841a8677f3
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2013-02-07 20:13:24 +0530

    Redis configuration.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -63,31 +63,33 @@ environments {
     development {
         grails.logging.jul.usebridge = true
 		grails.resources.debug = true  // Add this line in development environment.
-		redis {
-			poolConfig {
-				// jedis pool specific tweaks here, see jedis docs & src
-				// ex: testWhileIdle = true
+		grails {
+			redis {
+				poolConfig {
+					// jedis pool specific tweaks here, see jedis docs & src
+					// ex: testWhileIdle = true
+				}
+				port = 6379
+				host = "localhost"
+				timeout = 2000 //default in milliseconds
+				password = "somepassword" //defaults to no password
 			}
-			port = 6379
-			host = "localhost"
-			timeout = 2000 //default in milliseconds
-			username = //default in milliseconds
-			password = "somepassword" //defaults to no password
-		}
+        }
     }
     production {
         grails.logging.jul.usebridge = false
         // TODO: grails.serverURL = "http://www.changeme.com"
-		redis {
-			poolConfig {
-				// jedis pool specific tweaks here, see jedis docs & src
-				// ex: testWhileIdle = true
+		grails {
+			redis {
+				poolConfig {
+					// jedis pool specific tweaks here, see jedis docs & src
+					// ex: testWhileIdle = true
+				}
+				port = 9206
+				host = "dory.redistogo.com"
+				timeout = 20000 //default in milliseconds
+				password = "6bd1422203a6371dd8b27eaaf77b5322" //defaults to no password
 			}
-			port = 9206
-			host = "dory.redistogo.com"
-			timeout = 2000 //default in milliseconds
-			username = "redistogo" //default in milliseconds
-			password = "3610d0ea7ab754db58f1ca43fa8102d7" //defaults to no password
 		}
     }
 }
