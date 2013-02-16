b00f00e2c2ef521453e8fe5931eb57ff7bdbf635
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2013-02-16 20:34:04 +0530

    Partial commit for triggering job.

---
+++ grails-app/controllers/fonantrix/ChartController.groovy
@@ -0,0 +1,142 @@
+package fonantrix
+
+import groovy.json.JsonBuilder
+
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+import org.quartz.JobDataMap
+import org.quartz.JobKey
+import org.quartz.Scheduler
+import org.quartz.impl.matchers.GroupMatcher
+import org.springframework.context.ApplicationContext
+
+import redis.clients.jedis.Jedis
+
+
+class ChartController {
+
+	def redisService
+	def quartzScheduler
+
+	
+    def index() {
+		if (params.launchFromMain)
+		{
+			session.setAttribute("showMainSiteButton", true)
+		} else {
+			session.setAttribute("showMainSiteButton", false)
+		}
+
+		def chartList = Chart.list()
+		def mergeData = [];
+		
+		chartList.eachWithIndex { chart, index ->
+			def data = [
+				number : chart.number,
+				type : chart.type,
+				title : chart.title,
+				subtitle : chart.subtitle,
+				xAxisTitle : chart.xAxisTitle,
+				xAxisjson : chart.xAxisjson,
+				xAxisjson : chart.xAxisjson,
+				yAxistitle : chart.yAxistitle,
+				plotLinescolor : chart.plotLinescolor,
+				series : chart.seriess.collect {[
+					no: it.no,
+					type: it.type,
+					name: it.name,
+					data: it.dataValue,
+					additionalNodes : it.additionalNodes]}
+			]
+			mergeData.push(data)
+		}
+		def json = new JsonBuilder(mergeData)
+		
+		//System.out.println("mergeData:" + mergeData)
+		render(view: "/chart/index",  model:[charts:json.toString(),showMainSiteButton:"true"])
+	}
+	
+	def refreshData() {
+		//Jedis jedis = new Jedis("localhost")
+		def keys
+		redisService.withRedis { Jedis redis ->
+			keys = redis.keys("charts.*.series*.*Value");	
+			for (i in keys) {
+				int index
+				index = redis.llen(i);
+				def splitKey = i.split("\\.");
+				def chartNumber = splitKey[1]
+				def seriesNo = splitKey[2].substring(6)
+	
+				//loading chart based on extracted chart no
+				Chart chart = Chart.findByNumber(chartNumber)
+				//loading series of the above loaded chart
+				Series series = Series.findByChartAndNo(chart, seriesNo)
+				String data
+				if (index != 1) {
+					data = redis.lindex(i, index-1) 
+					data = Float.parseFloat(data) + 10
+					redis.rpush(i, data)
+				}
+				List<String> idList = redis.lrange(i, 0, -1)
+				series.setDataValue(idList.toListString())
+				series.save()
+			}
+		}
+		//val = jedis.lrange("charts.6.series2.dataValue",0,-1)
+		
+		redirect(controller: "chart", action: "index")
+	}
+	
+	def getDynamicData() {
+		
+		/*def runJob = {
+			//quartzScheduler.triggerJob( 'DynamicData', 'GettingData')
+			GetRandomDataJob.triggerNow();
+		}*/
+
+		//def job = jobManagerService.getJobs("")
+		//quartzScheduler.triggerJob(job, 'GettingData', params ? new JobDataMap(params) : null)
+		String myJobName = "fonantrix.GetRandomDataJob"
+		
+		ApplicationContext ctx = ServletContextHolder.getServletContext().getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)
+		
+		Scheduler scheduler = ctx.getBean("quartzScheduler")
+		
+System.out.println("name:" + scheduler)
+		
+def key = "charts." + params.chartNo + ".series"+ params.SerieNo + ".dataValue";
+
+		for (String groupName : scheduler.getJobGroupNames()) {
+
+System.out.println("groupName:" + groupName)
+			for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
+			
+				String jobName = jobKey.getName();
+				String jobGroup = jobKey.getGroup();
+System.out.println("jobName:" + jobName)
+				
+				if ((jobName).equals(myJobName)) {
+					scheduler.triggerJob(jobKey, new JobDataMap(["key":key]));
+					System.out.println("trigger job")
+				}
+			}
+		}
+
+		//Jedis jedis = new Jedis("localhost")
+
+		System.out.println("params.chartNo:" + params.chartNo)
+		System.out.println("params.SerieNo:" + params.SerieNo)
+		//loading chart based on extracted chart no
+		Chart chart = Chart.findByNumber(params.chartNo)
+		//loading series of the above loaded chart
+		Series series = Series.findByChartAndNo(chart, params.SerieNo)
+		List<String> idList
+		redisService.withRedis { Jedis redis ->
+			idList = redis.lrange(key,0,-1)
+			series.setDataValue(idList.toListString())
+			series.save()
+		}
+		render ( idList.toListString() + ", " + params.SerieNo)
+	}
+}
--- grails-app/jobs/fonantrix/GetRandomDataJob.groovy
+++ grails-app/jobs/fonantrix/GetRandomDataJob.groovy
@@ -1,24 +1,27 @@
 package fonantrix
 
-import redis.clients.jedis.Jedis;
+import org.quartz.JobExecutionContext
+
+import redis.clients.jedis.Jedis
 
 class GetRandomDataJob {
 	Random random = new Random()
 	def redisService
 	
-	def name  = 'DynamicData'
 	def group = 'GettingData'
 
 	static triggers = {}
 	
-    def execute() {
+    def execute(JobExecutionContext context) {
         // execute job
-System.out.println("inside GetRandomDataJob")
+//System.out.println("inside GetRandomDataJob" + context.getMergedJobDataMap())
+		def key = context.getMergedJobDataMap().get("key");
+//System.out.println("key:" + key)
 		redisService.withRedis { Jedis redis ->
 			int index = redis.llen(key);
 			if (index != 1) {
 				def data = redis.lindex(key, index-1)
-				data = Float.parseFloat(data) + random.nextInt(10 ** num)
+				data = Float.parseFloat(data) + random.nextInt(10)
 				redis.rpush(key, data.toString())
 			}
 		}
